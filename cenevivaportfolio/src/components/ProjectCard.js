404ad4a456d517a8de46303fef0bbae74917648b
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 16:42:09 -0400

    create project card component

--- cenevivaportfolio/src/components/ProjectCard.js
+++ cenevivaportfolio/src/components/ProjectCard.js
@@ -2,11 +2,29 @@ import React from "react";
 
 function ProjectCard(props) {
   return (
-    <div className="card container-fluid" style={{ width: "18rem" }}>
+    <div className="card container-fluid" style={{ width: "400px", marginBottom: "20px" }}>
       <img alt={props.name} class="card-img-top" src={props.image} />
       <div class="card-body">
-        <h5 className="card-title"> {props.name}</h5>
-        <p className="card-text"> {props.description}</p>
+        <h5 className="card-title" style={{fontFamily: "Roboto Slab, serif"}}> {props.name}</h5>
+        <p className="card-text" style={{fontFamily:"Montserrat, sans-serif"}}> {props.description}</p>
+        <div className="col-12">
+          <a
+            className="link row"
+            href={props.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Link
+          </a>
+          <a
+            className="link row"
+            href={props.github}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </div>
       </div>
     </div>
   );
