17bb49cd181598981593b36b3567ab9769c849a1
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 16:41:36 -0400

    add routes

--- cenevivaportfolio/src/App.js
+++ cenevivaportfolio/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Header";
 import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
+    <Router>
     <div>
-     <Project/>
+      <Navbar/>
+     <Route exact path="/" component={About}/>
+     <Route exact path="/project" component={Project}/>
+     <Route exact path="/contact" component={Contact}/>
+     <Footer/> 
     </div>
+    </Router>
   );
 }
 
