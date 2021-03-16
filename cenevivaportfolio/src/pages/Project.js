bafbc005dd3b1d67dba7122297ef77f32b592481
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 10:44:32 -0400

    add pages/project page.

---
+++ cenevivaportfolio/src/pages/Project.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+
+function Project() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default Project;
