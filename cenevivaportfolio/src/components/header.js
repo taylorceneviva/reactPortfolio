364afb8b6894c8c0e0b5cb0e57268e458a9cbce3
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 16:45:58 -0400

    add resume link

--- cenevivaportfolio/src/components/header.js
+++ cenevivaportfolio/src/components/header.js
@@ -1,21 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-      <a className="navbar-brand" href="/" style={{fontSize: "18px"}}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
         taylorceneviva.design
-      </a>
-      <a className="nav-item" href="/">
-        Projects
-      </a>
-      <a className="nav-item" href="/">
-        Contacts
-      </a>
-      <a className="nav-item" href="/">
-        Resume
-      </a>
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" ||
+                window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/project"
+              className={
+                window.location.pathname === "/project"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              contact
+            </Link>
+          </li>
+          <li className="nav-item">
+          <a
+            className= "nav-link"
+            href="https://www.dropbox.com/s/frkiyxbgab9j9r0/ceneviva.resume.pdf?dl=0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
