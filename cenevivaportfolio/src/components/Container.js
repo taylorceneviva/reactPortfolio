e4df2875df3f120114ce0081e0572fc74a11805b
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 10:48:22 -0400

    comment out for now

--- cenevivaportfolio/src/components/Container.js
+++ cenevivaportfolio/src/components/Container.js
@@ -1,12 +1,14 @@
-import React from "react";
-import Footer from "./Footer";
+// import React from "react";
+// import Footer from "./Footer";
+// import Navbar from "./Header";
 
-function Container() {
-  return (
-    <div>
-      <Footer/>
-    </div>
-  )
-}
+// function Container() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default Container;
+// export default Container;
