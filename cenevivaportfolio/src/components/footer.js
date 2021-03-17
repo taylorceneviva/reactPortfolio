09f9564cd7bde44a986dcd654e12ceed94c37209
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 23:09:23 -0400

    add style

--- cenevivaportfolio/src/components/footer.js
+++ cenevivaportfolio/src/components/footer.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer" style={{ fontSize: "18px", textAlign: "center", backgroundColor: "lightgray"}}>
-      <span>Taylor Ceneviva GitHub:@taylorceneviva &amp; OSU Coding Bootcamp</span>
+    <footer className="footer" style={{ position: "absolute" }}>
+      <span
+        style={{
+          fontSize: "18px",
+          textAlign: "center",
+          backgroundColor: "lightgray",
+          position: "fixed",
+          bottom: "0",
+          right: "0",
+          left: "0",
+          padding: "3px"
+        }}
+      >
+        Taylor Ceneviva GitHub:@taylorceneviva &amp; OSU Coding Bootcamp
+      </span>
     </footer>
   );
 }
