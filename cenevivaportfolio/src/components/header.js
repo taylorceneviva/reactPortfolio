4d1c1d6de7953fde5e53a6a7093c7aa44b264b5d
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 23:09:33 -0400

    add style

--- cenevivaportfolio/src/components/header.js
+++ cenevivaportfolio/src/components/header.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
+    <nav className="navbar navbar-expand-lg container-fluid"
+    style={{backgroundColor:"#F2E4DF"}}
+    >
+      <Link
+        className="navbar-brand"
+        style={{fontFamily: "Fjalla One, sans-serif", color:"#CEBEBA"}}
+        to="/"
+      >
         taylorceneviva.design
       </Link>
-      <div>
-        <ul className="navbar-nav">
+      <div className="col-6 container-fluid">
+        <ul
+          className="navbar-nav"
+          style={{ fontFamily: "Montserrat, sans-serif", color:"#CEBEBA"}}
+        >
           <li className="nav-item">
-            <Link
+            <Link style={{color:"#CEBEBA"}}
               to="/"
               className={
                 window.location.pathname === "/" ||
@@ -23,7 +32,7 @@ function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-            <Link
+            <Link style={{color:"#CEBEBA"}}
               to="/project"
               className={
                 window.location.pathname === "/project"
@@ -35,7 +44,7 @@ function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-            <Link
+            <Link style={{color:"#CEBEBA"}}
               to="/contact"
               className={
                 window.location.pathname === "/contact"
@@ -47,15 +56,39 @@ function Navbar() {
             </Link>
           </li>
           <li className="nav-item">
-          <a
-            className= "nav-link"
-            href="https://www.dropbox.com/s/frkiyxbgab9j9r0/ceneviva.resume.pdf?dl=0"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Resume
-          </a>
+            <a
+              className="nav-link"style={{color:"#CEBEBA"}}
+              href="https://www.dropbox.com/s/frkiyxbgab9j9r0/ceneviva.resume.pdf?dl=0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume
+            </a>
           </li>
+        </ul>
+      </div>
+      <div className="col-2 container-fluid">
+        <ul>
+          <ul className="nav-item">
+            <a
+              className="nav-link" style={{color:"#CEBEBA"}}
+              href="https://github.com/taylorceneviva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </ul>
+          <ul className="nav-item">
+            <a
+              className="nav-link" style={{color:"#CEBEBA"}}
+              href="https://www.linkedin.com/in/taylorceneviva/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </ul>
         </ul>
       </div>
     </nav>
