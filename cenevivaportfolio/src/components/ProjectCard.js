9d2374d237d24d377a08d742f2c877f1923ba42a
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 23:09:13 -0400

    add style

--- cenevivaportfolio/src/components/ProjectCard.js
+++ cenevivaportfolio/src/components/ProjectCard.js
@@ -2,11 +2,23 @@ import React from "react";
 
 function ProjectCard(props) {
   return (
-    <div className="card container-fluid" style={{ width: "400px", marginBottom: "20px" }}>
+    <div
+      className="card container-fluid"
+      style={{ width: "800px", marginBottom: "20px", backgroundColor:"#D9CAC5" }}
+    >
       <img alt={props.name} class="card-img-top" src={props.image} />
       <div class="card-body">
-        <h5 className="card-title" style={{fontFamily: "Roboto Slab, serif"}}> {props.name}</h5>
-        <p className="card-text" style={{fontFamily:"Montserrat, sans-serif"}}> {props.description}</p>
+        <h5 className="card-title" style={{ fontFamily: "Fjalla One, sans-serif" }}>
+          {" "}
+          {props.name}
+        </h5>
+        <p
+          className="card-text"
+          style={{ fontFamily: "Montserrat, sans-serif" }}
+        >
+          {" "}
+          {props.description}
+        </p>
         <div className="col-12">
           <a
             className="link row"
