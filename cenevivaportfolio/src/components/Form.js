406d51331c7680d417f23e5a37ff981778f9401b
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-03-16 23:08:27 -0400

    add style

--- cenevivaportfolio/src/components/Form.js
+++ cenevivaportfolio/src/components/Form.js
@@ -1,25 +1,42 @@
 import React from "react";
 
 function Form() {
-    return (
-        <form className="container-fluid">
-            <div className="mb-3" style={{ margin: "auto" }}>
-            <label for="exampleInputEmail1" class="form-label">Email address </label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1"/>
-            </div>
-            <div className="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    )
-
-};
+  return (
+    <form className="container-fluid" style={{ marginTop:"50px", width:"800px", backgroundColor:"#D9CAC5" }}>
+      <div className="mb-3">
+        <label for="form-txt">Name*</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="Your Name..."
+        />
+        <div id="emailHelp" class="form-text"></div>
+      </div>
+      <div className="mb-3">
+        <label for="form-txt">Email address*</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+      <div className="mb-3">
+        <label for="form-txt">Message*</label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary"
+      style={{backgroundColor:"#8C7765"}}
+      >
+        Send
+      </button>
+    </form>
+  );
+}
 
 export default Form;
